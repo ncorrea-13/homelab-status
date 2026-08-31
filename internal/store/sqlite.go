45fc3958d25566076f979969c865354196f6723e
ncorrea-13 <89622611+ncorrea-13@users.noreply.github.com>
2026-08-31 09:34:35 -0300

    feat: add sqlite factory

---
+++ internal/store/sqlite.go
@@ -0,0 +1,19 @@
+package store
+
+import (
+	"database/sql"
+
+	_ "modernc.org/sqlite"
+)
+
+type SQLiteStore struct {
+	db *sql.DB
+}
+
+func NewSQLiteStore(dbPath string) (*SQLiteStore, error) {
+	db, err := sql.Open("sqlite", dbPath)
+	if err != nil {
+		return nil, err
+	}
+	return &SQLiteStore{db: db}, nil
+}
