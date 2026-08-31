196ae900cf0e9c7195cd97971028c4bb61552c35
ncorrea-13 <89622611+ncorrea-13@users.noreply.github.com>
2026-08-31 13:58:25 -0300

    feat: add converts for string and int64

---
+++ internal/handlers/convert.go
@@ -0,0 +1,17 @@
+package handlers
+
+import "database/sql"
+
+func toNullString(s *string) sql.NullString {
+	if s == nil {
+		return sql.NullString{}
+	}
+	return sql.NullString{String: *s, Valid: true}
+}
+
+func toNullInt64(i *int64) sql.NullInt64 {
+	if i == nil {
+		return sql.NullInt64{}
+	}
+	return sql.NullInt64{Int64: *i, Valid: true}
+}
